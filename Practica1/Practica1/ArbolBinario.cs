3e2f25069221014f1146c04cc8f38acad0fc2eb9
emilioTalarico <emilio.talarico@hotmail.com>
2022-08-28 02:48:39 -0300

    Se agregó los métodos PreOrden, InOrden y PostOrden. Se dibujó en pantalla la estructura del arbol en cuestión.

---
+++ Practica1/Practica1/ArbolBinario.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    public class ArbolBinario<T>
+    {
+		/*Agregue a la clase ArbolBinario el siguiente método:
+		- contarHojas() #devuelve la cantidad de subárboles hojas del árbol receptor.*/
+
+		private T dato;
+		private ArbolBinario<T> hijoIzquierdo;
+		private ArbolBinario<T> hijoDerecho;
+
+
+		public ArbolBinario(T dato)
+		{
+			this.dato = dato;
+		}
+
+		public T getDatoRaiz()
+		{
+			return this.dato;
+		}
+
+		public ArbolBinario<T> getHijoIzquierdo()
+		{
+			return this.hijoIzquierdo;
+		}
+
+		public ArbolBinario<T> getHijoDerecho()
+		{
+			return this.hijoDerecho;
+		}
+
+		public void agregarHijoIzquierdo(ArbolBinario<T> hijo)
+		{
+			this.hijoIzquierdo = hijo;
+		}
+
+		public void agregarHijoDerecho(ArbolBinario<T> hijo)
+		{
+			this.hijoDerecho = hijo;
+		}
+
+		public void eliminarHijoIzquierdo()
+		{
+			this.hijoIzquierdo = null;
+		}
+
+		public void eliminarHijoDerecho()
+		{
+			this.hijoDerecho = null;
+		}
+
+		public bool esHoja()
+		{
+			return this.hijoIzquierdo == null && this.hijoDerecho == null;
+		}
+
+		//Se procesa el hijo izquierdo, luego la raíz y último el hijo derecho
+		public void inorden()
+		{
+            //Proceso hijo izquierdo
+            if (this.hijoIzquierdo != null)
+            
+				this.hijoIzquierdo.inorden();
+            
+			//Proceso raiz
+            Console.Write(this.dato + " ");
+            //Proceso hijo derecho
+            if (this.hijoDerecho != null)
+            
+				this.hijoDerecho.inorden();
+            
+		}
+
+		//Se procesa primero la raíz y luego sus hijos, izquierdo y	derecho
+		public void preorden()
+		{
+            //Proceso raiz
+            Console.Write(this.dato + " ");
+			//Proceso hijo izquierdo
+			if (this.hijoIzquierdo != null)
+			
+				this.hijoIzquierdo.preorden();
+			
+			//Proceso hijo derecho
+			if (this.hijoDerecho != null)
+			
+				this.hijoDerecho.preorden();
+
+			
+		}
+
+		//Se procesan primero los hijos, izquierdo y derecho, y luego la raíz
+		public void postorden()
+		{
+			//Proceso hijo izquierdo
+			if (this.hijoIzquierdo != null)
+			
+				this.hijoIzquierdo.postorden();
+			
+			//Proceso hijo derecho
+			if (this.hijoDerecho != null)
+			
+				this.hijoDerecho.postorden();
+			
+            //Proceso raiz
+            Console.Write(this.dato + " ");
+		}
+
+		//Se procesan los nodos teniendo en cuenta sus niveles, primero
+		//la raíz, luego los hijos, los hijos de éstos, etc.
+		public void recorridoPorNiveles()
+		{
+			//Proceso raiz
+            Console.Write(this.dato + " ");
+			//Proceso siguiente nivel
+			
+		}
+
+		public int contarHojas()
+		{
+			return 0;
+		}
+
+		public void recorridoEntreNiveles(int n, int m)
+		{
+		}
+	}
+}
--- Practica1/Practica1/Program.cs
+++ Practica1/Practica1/Program.cs
@@ -10,6 +10,43 @@ namespace Practica1
     {
         static void Main(string[] args)
         {
+
+            ArbolBinario<int> arbolBinarioA = new ArbolBinario<int>(1);
+
+            ArbolBinario<int> hijoIzquierdo = new ArbolBinario<int>(2);
+            hijoIzquierdo.agregarHijoIzquierdo(new ArbolBinario<int>(3));
+            hijoIzquierdo.agregarHijoDerecho(new ArbolBinario<int>(4));
+
+            ArbolBinario<int> hijoDerecho = new ArbolBinario<int>(5);
+            hijoDerecho.agregarHijoIzquierdo(new ArbolBinario<int>(6));
+            hijoDerecho.agregarHijoDerecho(new ArbolBinario<int>(7));
+
+            arbolBinarioA.agregarHijoIzquierdo(hijoIzquierdo);
+            arbolBinarioA.agregarHijoDerecho(hijoDerecho);
+
+            Console.WriteLine("\t\t\t\tRepresentacion del arbol\n");
+
+            Console.WriteLine("\t\t\t\t\t" + arbolBinarioA.getDatoRaiz());
+            Console.WriteLine("\t\t\t" + arbolBinarioA.getHijoIzquierdo().getDatoRaiz() + "\t\t\t\t" + arbolBinarioA.getHijoDerecho().getDatoRaiz());
+            Console.WriteLine("\t\t" + hijoIzquierdo.getHijoIzquierdo().getDatoRaiz() + "\t\t" + hijoIzquierdo.getHijoDerecho().getDatoRaiz() + "\t\t"
+                              + hijoDerecho.getHijoIzquierdo().getDatoRaiz() + "\t\t" + hijoDerecho.getHijoDerecho().getDatoRaiz()+"\n\n");
+
+
+            Console.Write("Preorden: ");
+            arbolBinarioA.preorden();
+            Console.WriteLine();
+            Console.Write("Inorden: ");
+            arbolBinarioA.inorden();
+            Console.WriteLine();
+            Console.Write("Postorden: ");
+            arbolBinarioA.postorden();
+            Console.WriteLine();
+            //Console.Write("Por Niveles: ");
+            //arbolBinarioA.recorridoPorNiveles();
+            //Console.WriteLine();
+
+            Console.ReadKey();
+
         }
     }
 }
